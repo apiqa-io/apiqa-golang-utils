221e56244e323b0f347fbf0fafc5cb018e0bc92f
ZmeevViktor <finhzv@gmail.com>
2019-12-30 16:45:27 +0300

    add rest errors

--- errors.go
+++ errors.go
@@ -1,1 +1,21 @@
 package golang_helpers
+
+type FieldError struct {
+	Field   string `json:"field"`
+	Message string `json:"message"`
+}
+
+type RestError struct {
+	Code    string     `json:"code"`
+	Message FieldError `json:"message"`
+}
+
+type RestErrors struct {
+	Code    string       `json:"code"`
+	Message []FieldError `json:"message"`
+}
+
+type UnknownRestError struct {
+	Code    string `json:"code"`
+	Message error  `json:"message,omitempty"`
+}
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package golang_helpers
 import (
 	"io"
 	"log"
+	"reflect"
 )
 
 func Close(c io.Closer) {
@@ -10,4 +11,14 @@ func Close(c io.Closer) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func Contains(slice interface{}, elem interface{}) bool {
+	sliceValue := reflect.ValueOf(slice)
+	for i := 0; i < sliceValue.Len(); i++ {
+		if sliceValue.Index(i).Interface() == reflect.ValueOf(elem).Interface() {
+			return true
+		}
+	}
+	return false
 }
